a08a3d07a2322ba19bef54d99c42ca965cd01dae
Andrey Klimenko <79120692680@yandex.ru>
2023-04-30 16:25:09 +0500

    dev | m1w6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { FC } from 'react'
+import { FC, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import './index.scss'
-import { Counter } from './components/Counter'
+
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { Link } from 'react-router-dom'
 
 export const App: FC = () => {
   return (
     <div className='app'>
-      Text
-      <Counter />
+      <Link to='/'>Главная</Link>
+      <Link to='/about'>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,9 @@
+import { lazy } from 'react'
+
+export const MainPageAsync = lazy(
+  () =>
+    new Promise(resolve => {
+      // @ts-ignore
+      setTimeout(() => resolve(import(/* webpackChunkName: "main_page"*/ './MainPage')), 500)
+    })
+)
