c1d1a53daf9a62be47341aece8e4fee2a34071fc
Andrey Klimenko <79120692680@yandex.ru>
2023-07-17 21:23:29 +0500

    module3 | w22v1

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,16 @@
+import { BugButton } from 'app/providers/ErrorBoundary'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const MainPage: FC = () => {
   const { t } = useTranslation('main')
 
-  return <div>{t('mainPage')}</div>
+  return (
+    <div>
+      <BugButton />
+      {t('mainPage')}
+    </div>
+  )
 }
 
 export default MainPage
