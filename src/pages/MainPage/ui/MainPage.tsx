b7c80ab00e9949d0d94dba7a7c27d00e2698d90f
Klimenko Andrey <klimenko.a@digital-spectr.ru>
2024-08-02 11:54:06 +0500

    add error boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,15 @@
+import { BugButton } from 'app/providers/ErrorBoundary'
 import { useTranslation } from 'react-i18next'
 
 function MainPage() {
   const { t } = useTranslation('main')
 
-  return <div>{t('mainPage')}</div>
+  return (
+    <div>
+      <BugButton />
+      {t('mainPage')}
+    </div>
+  )
 }
 
 export default MainPage
