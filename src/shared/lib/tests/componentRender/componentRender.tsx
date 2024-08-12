b8013608c829b01e9bd52fa0596316870508b3ad
Klimenko Andrey <klimenko.a@digital-spectr.ru>
2024-08-12 12:20:29 +0500

    add redux, add entity for counter

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,4 +1,6 @@
+import { DeepPartial } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
@@ -7,14 +9,17 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 
 export interface ComponentRenderOptions {
   route?: string;
+  initialState?: DeepPartial<StateSchema>;
 }
 
 export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
-  const { route = RoutePath.main } = options;
+  const { route = RoutePath.main, initialState } = options;
 
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTest}>{component}</I18nextProvider>
-    </MemoryRouter>
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTest}>{component}</I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>
   );
 }
