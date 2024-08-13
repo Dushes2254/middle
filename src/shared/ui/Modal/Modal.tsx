bb7586f936b097dfb22cbae705116425726a29f5
Klimenko Andrey <klimenko.a@digital-spectr.ru>
2024-08-13 10:33:46 +0500

    update theme for pages

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -61,7 +61,7 @@ export const Modal: FC<ModalProps> = ({ className, children, isOpen, onClose }) 
 
   return (
     <Portal>
-      <div className={classNames(cls.modal, mods, [className, theme])}>
+      <div className={classNames(cls.modal, mods, [className, theme, 'app_modal'])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>
             {children}
